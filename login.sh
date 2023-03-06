37f34e60604a5abd27fe713c315b440fcbe31e6e
Muhammed Ali <muhamzyali@gmail.com>
2023-03-06 17:23:41 +0100

    vv

--- login.sh
+++ login.sh
@@ -1,7 +1,7 @@
 expect -c 'spawn ssh root@109.74.202.103; expect {
                   "Are you sure you want to continue connecting" {
                       send "yes\r"
-                      interact
+                      exp_continue
                   }
                   "passwor" {
                       send "Muhammed-1998\r"
