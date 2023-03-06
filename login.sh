b00c5b7b9fe5c8d8ad45944c46dd4cf4c0abd057
Muhammed Ali <muhamzyali@gmail.com>
2023-03-06 14:22:32 +0100

    add interact to login

--- login.sh
+++ login.sh
@@ -1,9 +1,9 @@
 expect -c 'spawn ssh root@109.74.202.103; expect {
                   "Are you sure you want to continue connecting" {
                       send "yes\r"
-     
+                      interact
                   }
-                  "password:" {
+                  "passwor" {
                       send "Muhammed-1998\r"
                       interact
                   }
